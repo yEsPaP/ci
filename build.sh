3b1da77a9624fee4b4980908b32ba0a4ffdac4bc
yEsPaP <csgo40055@gmail.com>
2022-04-06 17:31:26 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,41 +1,38 @@
 #!/bin/bash
 
+# variables
 MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-9.0"
 DT_PATH=device/samsung/j7xelte
 DT_LINK="https://github.com/yespap/twrp.git -b android-9.0"
-
-mkdir ~/twrp
-cd ~/twrp
-apt update
-apt upgrade -y
-apt install libncurses5
 DEVICE=${DT_PATH##*\/}
 
-echo " ===+++ Syncing Recovery Sources +++==="
+# install needed tools
+apt update
+apt upgrade -y
+apt install libncurses5 bc
 mkdir -p ~/bin
 PATH="${HOME}/bin:${PATH}"
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
+# since we are building old tree, it thinks python2 is default
+rm /usr/bin/python
+ln -s /usr/bin/python2.7 /usr/bin/python
+
+# get the source
+mkdir ~/twrp
+cd ~/twrp
 repo init --depth=1 -u $MANIFEST -g default,-device,-mips,-darwin,-notdefault 
 repo sync -c -j$(nproc --all) --no-tag --no-clone-bundle --optimized-fetch --prune
 git clone --depth=1 $DT_LINK $DT_PATH
 rm -rf .repo
 find . -name ".git" | xargs rm
-ln -s /usr/bin/python2.7 /usr/bin/python
 
-echo " ===+++ Building Recovery +++==="
+# build
 source build/envsetup.sh
-echo " source build/envsetup.sh done"
 export ALLOW_MISSING_DEPENDENCIES=true
 export LC_ALL=C
 lunch omni_${DEVICE}-eng
 mka recoveryimage
 
-echo " ===+++ Uploading Recovery +++==="
-version=$(cat bootable/recovery/variables.h | grep "define TW_MAIN_VERSION_STR" | cut -d \" -f2)
-OUTFILE=TWRP-${version}-${DEVICE}-$(date "+%Y%m%d-%I%M").zip
-cd out/target/product/$DEVICE
-mv recovery.img ${OUTFILE%.zip}.img
-zip -r9 $OUTFILE ${OUTFILE%.zip}.img
-curl -T $OUTFILE https://oshi.at
-curl --upload-file $OUTFILE http://transfer.sh/
+# upload
+curl bashupload.com -T out/target/product/j7xelte/recovery.tar
